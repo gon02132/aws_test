67c94f808fd71c20061448078983fc2f0f7fd248
gon02132 <gon02132@naver.com>
2018-04-16 10:51:22 +0900

    2018-04-16

--- term/term_server.js
+++ term/term_server.js
@@ -1,25 +1,26 @@
 
-var express = require('express'); //일종의 프레임웤 ->파싱,세션관리..한꺼번에 저공
-var fs = require('fs'); //파일 읽고쓰기
-var app = express();
-var http = require('http').Server(app); //웹서버 객체 생성
-var io = require('socket.io').listen(http);
+var express       = require('express'); //일종의 프레임웤 ->파싱,세션관리..한꺼번에 저공
+var fs            = require('fs'); //파일 읽고쓰기
+var app           = express();
+var http          = require('http').Server(app); //웹서버 객체 생성
+var io            = require('socket.io').listen(http);
+
 var Global_values = {
-    ing: 0,
-    random_ing: 0,
-    random_check: 0,
-    eventSave: null,
-    isIntervalPlaying: 0,
-    head_location: "0.000",
-    tail_count: -1,
-    check: 0,
-    name_count: 2,
-    move: 0,
-    SleepTIme: 100,
-    bomb_count: 0,
-    fast_count: 0,
-    slow_count: 0,
-    long_count: 0
+    ing               : 0,
+    random_ing        : 0,
+    random_check      : 0,
+    eventSave         : null,
+    isIntervalPlaying : 0,
+    head_location     : "0.000",
+    tail_count        : -1,
+    check             : 0,
+    name_count        : 2,
+    move              : 0,
+    SleepTIme         : 100,
+    bomb_count        : 0,
+    fast_count        : 0,
+    slow_count        : 0,
+    long_count        : 0
 };
 
 //ing변수의         경우 Interval 제어(정지)를 위해 생성
@@ -43,6 +44,7 @@ app.get("/blank",function(req, res){
     res.end(data);
   });
 });
+
 //boom
 app.get("/boom",function(req, res){
   fs.readFile('boom.png', function (error,data) {
@@ -50,6 +52,7 @@ app.get("/boom",function(req, res){
     res.end(data);
   });
 });
+
 //fast
 app.get("/fast",function(req, res){
   fs.readFile('fast.png', function (error,data) {
@@ -57,6 +60,7 @@ app.get("/fast",function(req, res){
     res.end(data);
   });
 });
+
 //head
 app.get("/head",function(req, res){
   fs.readFile('head.png', function (error,data) {
@@ -64,6 +68,7 @@ app.get("/head",function(req, res){
     res.end(data);
   });
 });
+
 //my_head
 app.get("/my_head",function(req, res){
   fs.readFile('my_head.png', function (error,data) {
@@ -71,6 +76,7 @@ app.get("/my_head",function(req, res){
     res.end(data);
   });
 });
+
 //long_item
 app.get("/long_item",function(req, res){
   fs.readFile('long_item.png', function (error,data) {
@@ -78,6 +84,7 @@ app.get("/long_item",function(req, res){
     res.end(data);
   });
 });
+
 //slow
 app.get("/slow",function(req, res){
   fs.readFile('slow.png', function (error,data) {
@@ -85,6 +92,7 @@ app.get("/slow",function(req, res){
     res.end(data);
   });
 });
+
 //tail
 app.get("/tail",function(req, res){
   fs.readFile('tail.png', function (error,data) {
@@ -96,33 +104,44 @@ app.get("/tail",function(req, res){
 //접속한 클라이언트수
 var count = 0;
 
+//유저가 접속 했을시,
 io.on('connection', function(socket){
   count++;
   console.log('user connected: ', socket.id);
+
   //var name = "user" + count++;
   //io.to(socket.id).emit('change name',name);
 
+  //접속이 끊겼을 시,
   socket.on('disconnect', function(){
     count--;
     console.log('user disconnected: ', socket.id);
   });
 
+  //최초 맵 생성시, 유저들 맵 동기화
   socket.on('map size', function(map_size){
     console.log("mapsize:",map_size);
   });
+
+  //테이블  생성시, 유저들 맵 동기화
   socket.on('crt table', function (cr_tab, input) {
     io.emit('created table',cr_tab, input);
     //나포함 모든 소켓에게 전송
   });
+
+  //몸통 움직임 유저들에게 뿌려주기
   socket.on('move loc', function (head_location, imge) {
-    socket.broadcast.emit('moved loc', head_location, imge);
     //나를 제외한 모든 소켓에게 전송
+    socket.broadcast.emit('moved loc', head_location, imge);
   });
+
+  //꼬리 움직임 유저들에게 뿌려주기
   socket.on('tail move', function (tail_point) {
     socket.broadcast.emit('tail moved', tail_point);
   });
 });
 
+//포트번호 3000
 http.listen('3000', function(){
   console.log("server on!");
 });
